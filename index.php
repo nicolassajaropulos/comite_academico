02647fb5122aaa7d44d8b777476b1bfe2a88a6b7
nicolassajaropulos <nicolas.zavala.sajaropulos@gmail.com>
2018-04-26 19:18:34 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  	<head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Comité Academico</title>
+    	<link href="https://sitec.itculiacan.edu.mx/wp-content/themes/fuente/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+    	<link rel="stylesheet" href="librerias/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+  	</head>
+  	<body>
+  		<div class="row">
+	  		<div class="col-md-1 col-1">
+	  		</div>
+	   		<div class="col-md-10 col-10">
+	   			<div class="row">
+	   				<div class="col-md-1 col-1">
+		   			</div>
+		   			<div class="col-md-3 col-3 text-center">
+		   				<img src="imagen/header-sep.png">	
+		   			</div>
+		   			<div class="col-md-5 col-5 text-center">
+		   				</br>
+		   				<h4>TECNOLÓGICO NACIONAL DE MÉXICO</h4>
+		   				<h5>INSTITUTO TECNOLÓGICO DE CULIACÁN</h5>
+		   			</div>
+		   			<div class="col-md-3 col-3">
+		   				<img src="imagen/Logo-itc.png"><!--Imagen màs chica-->
+		   			</div>
+		   		</div>
+	   		</div>
+	   		<div class="col-md-1 col-1">
+	  		</div>
+	  	</div>
+  	</body>
+  	<script src="librerias/js/app.js"></script>
+</html>
---
+++ librerias/bootstrap-button-toggle/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/aanjulena/pen/ZLZjzV.
+
+ Working on a client project that will be using Bootstrap, and needed to build a toggle/switch that came in a couple different sizes. I wasn't seeing that as a built-in option within Bootstrap, and I didn't really like the ones already out there that I found from a quick search (I didn't want to use a plugin, just wanted it to utilize Bootstrap's built-in JS), so I decided to make my own.
+
+This example is just using the default "Quick Add" Bootstrap CSS & JS, no added/custom JS on my part, I've simply utilized Bootstrap's buttons.js Single Toggle method (http://getbootstrap.com/javascript/#buttons) and added some custom styling (LESS).
