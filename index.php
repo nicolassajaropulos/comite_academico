2803405318b3f1d20b3cd1eab8c958c432cd9d59
Diego Armando Payán López <DiegoGMR@users.noreply.github.com>
2018-04-29 00:38:32 -0600

    Avance de diseño

--- index.php
+++ index.php
@@ -7,31 +7,122 @@
     	<title>Comité Academico</title>
     	<link href="https://sitec.itculiacan.edu.mx/wp-content/themes/fuente/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
     	<link rel="stylesheet" href="librerias/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+		<link rel="stylesheet" href="css/general.css">
   	</head>
   	<body>
-  		<div class="row">
-	  		<div class="col-md-1 col-1">
-	  		</div>
-	   		<div class="col-md-10 col-10">
+  		<div class="row login_header hidden-sm-down">
+	   		<div class="col-md-12 col-12"><br>
 	   			<div class="row">
-	   				<div class="col-md-1 col-1">
+		   			<div class="col-md-3 col-3 logo_sep hidden-sm-down">
+		   				<img src="imagenes/header-sep.png">	
 		   			</div>
-		   			<div class="col-md-3 col-3 text-center">
-		   				<img src="imagen/header-sep.png">	
-		   			</div>
-		   			<div class="col-md-5 col-5 text-center">
+		   			<div class="col-md-6 col-6 col-sm-12 text-center text-tnm">
 		   				</br>
-		   				<h4>TECNOLÓGICO NACIONAL DE MÉXICO</h4>
-		   				<h5>INSTITUTO TECNOLÓGICO DE CULIACÁN</h5>
+		   				<h4><strong class="title">TECNOLÓGICO NACIONAL DE MÉXICO</strong></h4>
+		   				<h5><strong class="title">INSTITUTO TECNOLÓGICO DE CULIACÁN</strong></h5>
+						</br></br>
 		   			</div>
-		   			<div class="col-md-3 col-3">
-		   				<img src="imagen/Logo-itc.png"><!--Imagen màs chica-->
+		   			<div class="col-md-3 col-3 hidden-sm-down logo_itc">
+		   				<img src="imagenes/Logo-itc.png">
 		   			</div>
 		   		</div>
 	   		</div>
-	   		<div class="col-md-1 col-1">
-	  		</div>
 	  	</div>
+		<div class="row hidden-sm-down">
+			<div class="col-md-2 col-2">
+			</div>
+			<div class="col-md-8 col-8 login_panel text-center">
+				<div class="row">
+					<div class="col-md-6 col-6 banner_izq_div">
+						<a href="http://itculiacan.edu.mx/" class="banner_izq"> Página principal</a>
+					</div>
+					<div class="col-md-6 col-6 banner_der_div">
+						<a href="#" class="banner_der"> Iniciar sesión</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 col-2">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2 col-2">
+			</div>
+			<div class="col-md-8 col-8 col-sm-12"><br>
+				<div class="row">
+					<div class="col-md-5 col-5 hidden-sm-down body_text">
+						<h1>Bienvenido al Sistema de Comité académico.</h1>
+						<h5>En este sistema, se llevará a cabo al realización y el proceso de la solicitudes que correspondan al comité académico.</h5><br>
+						<h5><i class="fas fa-desktop"></i> Para acceder, utiliza tu Número de Control correspondiente y tu contraseña. </h5><br>
+						<h5><i class="fas fa-unlock-alt"></i> Si tu sesión es nueva, deberás utilizar una contraseña representada por una sucesión numérica del 1 al 8, una vez inicies sesión, 
+							asegúrate de dirijirte al módulo de cambio de contraseña para garantizar la privacidad y seguridad de tus datos.</h5><br>
+						<span style="color:#00BFFF;font-size:22px;font-weight: 500;"><i class="fas fa-phone"></i> Para soporte</span>
+						<span style="font-size:22px;font-weight: 500;"> Llame al teléfono +52 (667) 266-72-00</span>	
+					</div>
+					<div class="col-md-1 col-1 separator hidden-sm-down">
+					</div>
+					<div class="col-md-6 col-12 col-sm-12">
+						<div class="card">
+							<div class="card-header login_head">
+								<h3 class="text-center">Iniciar Sesión</h3>
+							 </div>
+							<div class="card-body login_body"><br>
+								<div class="row">
+									<div class="col-md-1 col-1 hidden-sm-down"></div>
+									<div class="col-md-10 col-12 col-sm-12">
+										<div class="col-md-12 col-12 col-sm-12">
+											<div class="form-group">
+												<h5><i class="fas fa-user"></i> Número de control </h5>
+												<input type="text" class="form-control" id="txt_usuario" placeholder="Número de control">
+											</div>
+										</div>
+										<div class="col-md-12 col-12 col-sm-12">
+											<div class="form-group">
+												<h5><i class="fas fa-key"></i> Contraseña </h5>
+												<input type="password" class="form-control" id="txt_password" placeholder="Contraseña">
+											</div>
+										</div>
+										<div class="col-md-12 col-12 col-sm-12"><hr>
+											<div class="form-group">
+												<div class="card recatcha">
+													<div class="col-md-12 col-12 col-sm-12 mt-3 ml-3">
+														<div class="row">
+															<div class="checkbox">
+																<label style="font-size: 30px">
+																	<input type="checkbox" id="check_human">
+																	<span class="cr"><i class="cr-icon fa fa-check"></i></span>
+																</label>
+															</div>
+															<h5 class="mt-2"> No soy un robot <i class="fas fa-male"></i> <i class="fas fa-female"></i></h5>
+															<img src="imagenes/RecaptchaLogo.png">
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="col-md-12 col-12 col-sm-12"><hr>
+											<button class="btn btn-outline-primary mb-3"><i class="fas fa-sign-in-alt"></i> Iniciar sesión</button>
+										</div>
+									</div>
+									<div class="col-md-1 col-1"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 col-2 hidden-sm-down">
+			</div>
+		</div>
+		<div class="footer hidden-sm-down">
+			<div class="col-md-12 col-12 text-center">
+				<label class="mb-0">Instituto Tecnológico de Culiacán</label><br>
+				<label class="mb-0">Av. Juan de Dios Bátiz 310 Pte. Col. Guadalupe. Culiacán, Sinaloa. C.P. 80220 Teléfono: +52 (667) 266-72-00 </label><br>
+				<label class="mb-0">Tecnológico Nacional de México – Algunos derechos reservados © 2014-2018</label><br>
+				<label class="mb-0">Política de Privacidad</label><br><br>
+				<label class="mb-0">Comité académico V1.0</label>
+			</div>
+		</div>
   	</body>
-  	<script src="librerias/js/app.js"></script>
+  	<!--script src="librerias/js/app.js"></script-->
 </html>
---
+++ view/main_page.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  	<head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Comité Academico</title>
+    	<link href="https://sitec.itculiacan.edu.mx/wp-content/themes/fuente/images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+    	<link rel="stylesheet" href="../librerias/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+		<link rel="stylesheet" href="../css/general.css">
+  	</head>
+  	<body>
+  		<div class="row login_header hidden-sm-down">
+	   		<div class="col-md-12 col-12"><br>
+	   			<div class="row">
+		   			<div class="col-md-3 col-3 logo_sep hidden-sm-down">
+		   				<img src="../imagenes/header-sep.png">	
+		   			</div>
+		   			<div class="col-md-6 col-6 col-sm-12 text-center text-tnm">
+		   				</br>
+		   				<h4><strong class="title">TECNOLÓGICO NACIONAL DE MÉXICO</strong></h4>
+		   				<h5><strong class="title">INSTITUTO TECNOLÓGICO DE CULIACÁN</strong></h5>
+						</br></br>
+		   			</div>
+		   			<div class="col-md-3 col-3 hidden-sm-down logo_itc">
+		   				<img src="../imagenes/Logo-itc.png">
+		   			</div>
+		   		</div>
+	   		</div>
+	  	</div>
+		<div class="row">
+			<div class="col-md-2 col-2">
+			</div>
+			<div class="col-md-8 col-8 login_panel">
+			
+				<nav class="nav nav_bar_presidente">
+				
+					<a class="nav-link active" href="#"><i class="fa fa-calendar-alt"></i> Agendar Reuniones</a>
+					<img src="../imagenes/dot.png">
+					<a class="nav-link" href="#"><i class="fa fa-folder-open"></i> Orden del día</a>
+					
+					<div class="dropdown ml-auto dropdown_user">
+						<button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-user"></i> Presidente
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#"><i class="fa fa-pencil-alt"></i> Actualizar datos</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-unlock-alt"></i> Actualizar contraseña</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-times"></i> Cerrar sesión</a>
+						</div>
+					</div>
+					
+				</nav>
+				
+				<nav class="nav nav_bar_secretario">
+				
+					<a class="nav-link active" href="#"><i class="fa fa-clipboard"></i> Acta de acuerdos</a>
+					<img src="../imagenes/dot.png">
+					<a class="nav-link" href="#"><i class="fa fa-list-alt"></i> Control de asistencia</a>
+					
+					<div class="dropdown ml-auto dropdown_user">
+						<button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-user"></i> Secretario
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#"><i class="fa fa-pencil-alt"></i> Actualizar datos</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-unlock-alt"></i> Actualizar contraseña</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-times"></i> Cerrar sesión</a>
+						</div>
+					</div>
+					
+				</nav>
+				
+				<nav class="nav nav_bar_miembro">
+				
+					<a class="nav-link active" href="#"><i class="fa fa-calendar-check"></i> Revisar agenda de reuniones</a>
+					<img src="../imagenes/dot.png">
+					<a class="nav-link" href="#"><i class="fa fa-upload"></i> Registro de evidencias</a>
+					
+					<div class="dropdown ml-auto dropdown_user">
+						<button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-user"></i> Miembro
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#"><i class="fa fa-pencil-alt"></i> Actualizar datos</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-unlock-alt"></i> Actualizar contraseña</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-times"></i> Cerrar sesión</a>
+						</div>
+					</div>
+					
+				</nav>
+				
+				<nav class="nav nav_bar_interesado">
+				
+					<a class="nav-link active" href="#"><i class="fa fa-newspaper"></i> Ingresar nueva solicitud</a>
+					<img src="../imagenes/dot.png">
+					<a class="nav-link" href="#"><i class="fa fa-tasks"></i> Estado de solicidud</a>
+					
+					<div class="dropdown ml-auto dropdown_user">
+						<button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-user"></i> Interesado
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#"><i class="fa fa-pencil-alt"></i> Actualizar datos</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-unlock-alt"></i> Actualizar contraseña</a>
+							<a class="dropdown-item" href="#"><i class="fa fa-times"></i> Cerrar sesión</a>
+						</div>
+					</div>
+					
+				</nav>
+				
+			</div>
+			<div class="col-md-2 col-2">
+			</div>
+		</div>
+		
+  	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="../librerias/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</html>
