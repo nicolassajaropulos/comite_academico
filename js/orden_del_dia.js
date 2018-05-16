f73351f7e7138ae169425f2f160228329f3b69fc
nicolassajaropulos <nicolas.zavala.sajaropulos@gmail.com>
2018-05-15 23:24:27 -0600

    15/05/2018
    Filtros en orden del dia

--- js/orden_del_dia.js
+++ js/orden_del_dia.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	
 	$('.carga_solicitudes').hide();
+	$('.back-action').hide();
 	
 	$.ajax({
 		url: '../api/api.php/reuniones',
@@ -9,7 +10,7 @@ $(document).ready(function(){
 		success:function(data){
 			$(data.data).each(function(i,v){
 				$('.carga_reuniones').append('<tr>'
-											  +'<td>'+(i+1)+'</td>'
+											  +'<th scopre="row">'+(i+1)+'</th>'
 											  +'<td>'+v.nombre_convocador+'</td>'
 											  +'<td>'+(v.fecha).substring(0, 10)+'</td>'
 											  +'<td>'+v.hora_inicio+'</td>'
@@ -44,6 +45,79 @@ $(document).ready(function(){
 		
 		$('.carga_reuniones').hide();
 		$('.carga_solicitudes').fadeIn('fast');
+		$('.back-action').show('fast');
+		$('#btn_aplicar_filtro').data('table', '1');
+	});
+
+	// MENU DE NAVEGACIÓN DE LA DERECHA
+
+	$('#btn_aceptar').click(function(){
+		// Accion para btn_aceptar
+	});
+
+	$('#btn_regresar').click(function(){
+		$('.back-action').hide('fast');
+		$('.carga_solicitudes').hide();
+		$('.carga_reuniones').show('slow');
+		$('#btn_aplicar_filtro').data('table', '0');
+	});
+
+	// MODAL PARA FILTROS
+
+	$('.datepicker').datepicker({
+	    format: 'dd/mm/yyyy',
+	    language: 'es',
+	    todayBtn: "linked",
+	    autoclose: true,
+	    todayHighlight: true
+	});
+
+	$('#btn_limpiar_filtro').click(function(){
+		$('#txt_fecha_filtro').val('');
+		$('#txt_convocador_filtro').val('');
+		$('#btn_aplicar_filtro').click();
+	});
+
+	$('#btn_aplicar_filtro').click(function(){
+
+		var date_filter = $.trim($('#txt_fecha_filtro').val().toUpperCase());
+		var name_filter = $.trim($('#txt_convocador_filtro').val().toUpperCase());
+		var name = "", date = "";
+
+		if ($(this).data('table') == 0) {
+
+			$('.carga_reuniones tbody tr').each(function(){
+
+				name = $(this).find("td").eq(0).html().toUpperCase();
+				date = $(this).find("td").eq(1).html().toUpperCase();
+
+				if ((date_filter == "" || date.indexOf(date_filter) > -1) && (name_filter == "" || name.indexOf(name_filter) > -1)){
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+
+			});
+
+		} else {
+			
+			$('.carga_solicitudes tbody tr').each(function(){
+
+				name = $(this).find("td").eq(0).html().toUpperCase();
+				date = $(this).find("td").eq(2).html().toUpperCase();
+
+				if ((date_filter == "" || date.indexOf(date_filter) > -1) && (name_filter == "" || name.indexOf(name_filter) > -1)){
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+
+			});
+
+		}
+
+		$('#dlg_filtro').modal('hide');
+
 	});
 	
 });
--- view/orden_del_dia.php
+++ view/orden_del_dia.php
@@ -1,22 +1,19 @@
-<div class="col-md-12 col-12 mt-3 ml-5">
+<div class="col-12">	
 	<div class="row">
-		<h2>Orden del día</h2>
+		<div class="col-md-12 col-12 mt-3 ml-5">
+			<div class="row">
+				<h2>Orden del día</h2>
+			</div>
+			<div class="row">
+				<h5>Utilice este módulo para consultar las solicitudes del día</h5>
+			</div>
+		</div>
 	</div>
 	<div class="row">
-		<h5>Utilice este módulo para consultar las solicitudes del día</h5>
-	</div>
-</div>
-<div class="col-md-12 col-12">
-
-	<div class="row">
-	
-		<div class="col-md-12 mt-2">
-		
+		<div class="col-md-10 col-10">
 			<div class="card mod_panel">
-				
 				<div class="row ml-2">
 					<div class="col-md-11 ml-5">
-						
 						<table class="table carga_reuniones mt-3 table-striped table-hover">
 							<thead>
 								<tr>
@@ -28,11 +25,9 @@
 									<th scope="col"></th>
 								</tr>
 							</thead>
-							<tbody>
-								
+							<tbody>							
 							</tbody>
-						</table>
-						
+						</table>	
 						<table class="table carga_solicitudes mt-3 table-striped table-hover">
 							<thead>
 								<tr>
@@ -45,24 +40,78 @@
 							</thead>
 							<tbody>
 								<tr>
-								  <th scope="row">1</th>
-								  <td>Jesús Arturo Ayón</td>
-								  <td>Cambio de calificación</td>
-								  <td>09/05/2018</td>
-								  <td><button class="btn btn-info btn-sm"><i class="fa fa-arrow-circle-up"></i></button><button class="btn btn-info btn-sm"><i class="fa fa-arrow-circle-down"></i></button></td>
+									<th scope="row">1</th>
+								  	<td>Jesús Arturo Ayón</td>
+								  	<td>Cambio de calificación</td>
+								  	<td>09/05/2018</td>
+								  	<td><button class="btn btn-info btn-sm"><i class="fa fa-arrow-circle-up"></i></button><button class="btn btn-info btn-sm"><i class="fa fa-arrow-circle-down"></i></button></td>
 								</tr>
-							</tbody>
-						</table>
-						
+								</tbody>
+						</table>	
 					</div>
-				</div>
-				
+				</div>	
 			</div>
-			
 		</div>
-		
+		<div class="col-md-2 mt-2">
+			<div class="card text-center button_panel">
+				<div class="col-md-12 col-12 mt-2">
+					<h4>Acciones</h4><hr>
+				</div>
+				<div class="col-md-12 col-12 mt-2 mb-2">
+					<button class="btn btn-outline-success btn-block" id="btn_aceptar"> Aceptar</button>
+				</div>
+				<div class="col-md-12 col-12 mt-2 mb-2 back-action">
+					<button class="btn btn-outline-danger btn-block" id="btn_regresar"> Regresar</button>
+				</div>
+				<div class="col-md-12 col-12 mt-2 mb-2">
+					<button class="btn btn-outline-info btn-block" id="btn_filtrar" data-toggle="modal" data-backdrop="static" data-keyboard="false" data-target="#dlg_filtro"> Filtrar</button>
+				</div>
+			</div>
+		</div>
 	</div>
-	
+</div>
+
+<!-- MODAL FILTRO -->
+
+<div class="modal fade" id="dlg_filtro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+      		<div class="modal-header">
+        		<h5 class="modal-title" id="exampleModalLabel">Filtrar por:</h5>
+        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          			<span aria-hidden="true">&times;</span>
+        		</button>
+      		</div>
+	      	<div class="modal-body text-center">
+	      		<div class="row">
+	      			<div class="col-md-1"></div>
+			  		<div class="col-md-6">
+				      	<div class="form-group">
+				            <label for="txt_convocador_filtro">Convocador:</label>
+				            <input type="text" class="form-control text-center" id="txt_convocador_filtro">
+				        </div>
+			  		</div>
+			  		<div class="col-md-4">
+				      	<div class="form-group">
+				            <label for="txt_fecha_filtro">Fecha:</label>
+				            <input type="text" class="form-control text-center datepicker" id="txt_fecha_filtro">
+				      	</div>
+			  		</div>
+	      		</div>
+	      		<div class="row">
+	      			<div class="col-md-6">
+	      				
+	      			</div>
+	      			<div class="col-md-6"></div>
+	      		</div>
+	      	</div>
+	      	<div class="modal-footer">
+	        	<button type="button" class="btn btn-outline-danger" data-dismiss="modal"><i class="fa fa-reply" aria-hidden="true"></i> Cerrar</button>
+	        	<button type="button" class="btn btn-outline-warning" id="btn_limpiar_filtro"><i class="fa fa-eraser" aria-hidden="true"></i> Limpiar</button>
+	        	<button type="button" class="btn btn-outline-primary" id="btn_aplicar_filtro" data-table="0"><i class="fas fa-search"></i> Filtrar</button>
+	      	</div>
+    	</div>
+  	</div>
 </div>
 
 <script type="text/javascript" src="../js/orden_del_dia.js"></script>
