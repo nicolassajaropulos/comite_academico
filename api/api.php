ad88c6671f88531e9c9bb1f4391508149e5e9dc2
Diego Armando Payán López <DiegoGMR@users.noreply.github.com>
2018-05-21 09:40:45 -0600

    21/05/2018
    Disque Fix Metodo PUT

--- api/api.php
+++ api/api.php
@@ -63,7 +63,8 @@
 		
 	}else if($metodo == 'PUT'){
 		
-		$_SERVER['REQUEST_METHOD'] === 'PUT' ? parse_str(file_get_contents('php://input', false , null, -1 , $_SERVER['CONTENT_LENGTH'] ), $_PUT): $_PUT = array();
+		parse_str(file_get_contents('php://input'), $_PUT);
+		var_dump($_PUT);
 		
 		$response = array();
 		
