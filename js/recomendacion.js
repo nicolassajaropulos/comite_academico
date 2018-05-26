ba22ac942beb46404faae75ef96ed77e062bbd8f
Diego Armando Payán López <DiegoGMR@users.noreply.github.com>
2018-05-25 21:34:05 -0600

    25/05/2018
    Módulo de recomendación "Finalizado"

--- js/recomendacion.js
+++ js/recomendacion.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 											  +'<td>'+(v.fecha).substring(0, 10)+'</td>'
 											  +'<td>'+v.hora_inicio+'</td>'
 											  +'<td>'+v.hora_fin+'</td>'
-											  +'<td class="text-center"><button class="btn btn-info btn-sm" data-id="'+v.id_reunion+'" id="btn_recomendacion"><i class="fa fa-list-alt"></i> Solicitudes</button></td>'
+											  +'<td class="text-center"><button class="btn btn-info btn-sm" data-id="'+v.id_reunion+'" id="btn_solicitudes"><i class="fa fa-list-alt"></i> Solicitudes</button></td>'
 											+'</tr>');
 			});
 		},
@@ -26,12 +26,12 @@ $(document).ready(function(){
 
 	});
 	
-	$('.carga_reuniones').on("click","#btn_recomendacion",function(){
+	$('.carga_reuniones').on("click","#btn_solicitudes",function(){
 		
 		var id_reunion = $(this).data("id");
 		
 		$.ajax({
-			url: '../api/api.php/solicitudes_reunion?id_reunion='+id_reunion,
+			url: '../api/api.php/solicitudes_reunion_recomendacion?id_reunion='+id_reunion,
 			type: 'GET',
 			dataType: 'JSON',
 			success:function(data){
@@ -42,7 +42,8 @@ $(document).ready(function(){
 												  +'<td>'+v.nombre_usuario+'</td>'
 												  +'<td>'+v.fecha_creacion+'</td>'
 												  +'<td class="text-center">'
-												  +'<button class="btn btn-info btn-sm mr-1" data-id="'+v.id_solicitud+'" id="btn_visualizar" data-user="'+v.nombre_usuario+'" data-priori="'+v.prioridad+'"><i class="fa fa-clipboard-check"></i> Recomendacion</button>'
+												  +'<button class="btn btn-info btn-sm" data-id="'+v.id_solicitud+'" id="btn_visualizar" data-user="'+v.nombre_usuario+'" data-priori="'+v.prioridad+'"><i class="fa fa-search"></i> Visualizar</button>'
+												  +'<button class="btn btn-primary btn-sm ml-1" data-id="'+v.id_solicitud+'" id="btn_recomendacion" data-user="'+v.nombre_usuario+'" data-priori="'+v.prioridad+'"><i class="fa fa-clipboard-check"></i> Recomendacion</button>'
 												  +'</td>'
 												+'</tr>');
 				});
@@ -58,6 +59,156 @@ $(document).ready(function(){
 		$('.carga_solicitudes').fadeIn('fast');
 		$('.back-action').show('fast');
 		$('#btn_aplicar_filtro').data('table', '1');
+	});
+	
+	$('.carga_solicitudes').on("click","#btn_recomendacion",function(){
+		
+		var id_solicitud = $(this).data('id');
+		
+		$.ajax({
+			url: '../api/api.php/cuenta_validacion_solicitud?id_solicitud='+id_solicitud,
+			type: 'GET',
+			dataType: 'JSON',
+			success:function(data){
+				
+				$(data.data).each(function(i,v){
+					$('.numero_aceptadas').html('<h5><strong>Votos positivos:</strong> ' + v['aceptadas'] + '</h5>');
+					$('.numero_rechazadas').html('<h5><strong>Votos negativos:</strong> ' + v['rechazadas'] + '</h5>');
+					
+					var solicitud_estatus = "";
+					
+					if( v['aceptadas'] > v['rechazadas']){
+						solicitud_estatus = "Aceptada";
+					}else if(v['rechazadas'] > v['aceptadas']){
+						solicitud_estatus = "Rechazada";
+					}else{
+						solicitud_estatus = "Votación igualada, requiere voto de calidad.";
+					}
+					
+					$('.solicitud_estatus').html('<h5><strong>Desición comité:</strong> ' + solicitud_estatus + '</h5>');
+					
+				});
+				
+				$('#btn_almacenar_recomendacion').data('id', id_solicitud);
+				
+			},
+			error: function(xhr, desc, err){
+				console.log(xhr);
+				console.log("Detalles: " + desc + "\nError: " + err);
+			}
+		});
+		
+		$('#modal_recomendacion').modal('show');
+	
+	});
+	
+	$('.carga_solicitudes').on("click","#btn_visualizar",function(){
+		
+		var prioridad_interesado = $(this).data("priori");
+		
+		if(prioridad_interesado == 4){
+			$('#modal_visualizar_solicitud_estudiante').modal('toggle');
+		}else{
+			$('#modal_visualizar_solicitud_profesor').modal('toggle');
+		}
+		
+		$('.titulo_modal').html("Vista de Solicitud No. <strong>" + $(this).data('id') + "</strong>");
+		
+		$.ajax({
+			url: "../api/api.php/datos_solicitud",
+			type: "GET",
+			dataType: "JSON",
+			data: {
+				"id_solicitud" : $(this).data('id')
+			},
+			success: function(data){
+				
+				$(data.data).each(function(i,v){
+					
+					if(prioridad_interesado == 4){
+						
+						$('#txt_nombre_estudiante').val(v.nombre_usuario);
+						$('#txt_carrera_estudiante').val(v.nombre_carrera);
+						$('#txt_solicitud_estudiante').val(v.solicitud);
+						$('#txt_motivos_academicos_estudiante').val(v.motivo_academico);
+						$('#txt_motivos_personales_estudiante').val(v.motivo_personal);
+						$('#txt_motivos_otros_estudiante').val(v.motivo_otro);
+						
+						$('#txt_nombre_estudiante').prop('readonly', true);
+						$('#txt_carrera_estudiante').prop('readonly', true);
+						$('#txt_solicitud_estudiante').prop('readonly', true);
+						$('#txt_motivos_academicos_estudiante').prop('readonly', true);
+						$('#txt_motivos_personales_estudiante').prop('readonly', true);
+						$('#txt_motivos_otros_estudiante').prop('readonly', true);
+						
+					}else{
+						
+						$('#txt_nombre_profesor').val(v.nombre_usuario);
+						$('#txt_carrera_profesor').val(v.nombre_carrera);
+						$('#txt_solicitud_profesor').val(v.solicitud);
+						$('#txt_motivos_academicos_profesor').val(v.motivo_academico);
+						
+						$('#txt_nombre_profesor').prop('readonly', true);
+						$('#txt_carrera_profesor').prop('readonly', true);
+						$('#txt_solicitud_profesor').prop('readonly', true);
+						$('#txt_motivos_academicos_profesor').prop('readonly', true);
+						
+					}
+					
+				});
+				
+			},
+			error: function(xhr, desc, err){
+				console.log(xhr);
+				console.log("Descripcion: " + desc + "\nError: "  + err);
+			}
+		});
+		
+	});
+	
+	$('#btn_almacenar_recomendacion').click(function(){
+		
+		var id_solicitud = $(this).data('id');
+		var comentario = $('#txt_recomendacion').val();
+		
+		$.ajax({
+			url: "../api/api.php/comentario_solicitud",
+			type: "POST",
+			data: {
+				"id_solicitud" : id_solicitud,
+				"comentario" : comentario,
+				"estatus" : '4'
+			},
+			success: function(data){
+				
+				setTimeout(function(){
+					swal({
+						title: "Exito!", 
+						text: "Recomendacion almacenada",
+						type: "success",
+						timer: 2000
+					});
+				},200);
+				
+				setTimeout(function(){
+					cargarDiv("recomendacion");
+				},2000);
+				
+			},
+			error: function(xhr, desc, err){
+				console.log(xhr);
+				console.log("Descripcion: " + desc + "\nError: "  + err);
+			}
+		});
+		
+	});
+	
+	$('#btn_regresar').click(function(){
+		$('.back-action').hide('fast');
+		$('.carga_solicitudes').hide();
+		$('.carga_reuniones').fadeIn('slow');
+		$('#btn_aplicar_filtro').data('table', '0');
+		$(".carga_solicitudes tbody tr").remove();
 	});
 	
 });
--- view/recomendacion.php
+++ view/recomendacion.php
@@ -76,26 +76,160 @@
 						<div class="col-md-12 col-12">
 							<div class="form-group">
 								<h5>Recomendación:</h5>
-								<textarea class="form-control" id="txt_comentario" placeholder="Recomendacion" rows="5"></textarea>
+								<textarea class="form-control" id="txt_recomendacion" placeholder="Recomendacion" rows="5"></textarea>
 							</div>
 						</div>
 					</div>
 					<div class="row">
 						<div class="col-md-6 col-6">
-							<div class="alert alert-info" role="alert">
-							  1
+							<div class="alert alert-success numero_aceptadas" role="alert">
 							</div>
 						</div>
 						<div class="col-md-6 col-6">
-							<div class="alert alert-danger" role="alert">
-							  2
+							<div class="alert alert-danger numero_rechazadas" role="alert">
 							</div>
 						</div>
 					</div>
 					<div class="row">
 						<div class="col-md-12 col-12">
-							<div class="alert alert-primary" role="alert">
-							 3
+							<div class="alert alert-info solicitud_estatus" role="alert">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+				<button type="button" class="btn btn-success" data-dismiss="modal" id="btn_almacenar_recomendacion"><strong><i class="fa fa-check-circle"></i> Enviar</strong></button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal" id="modal_visualizar_solicitud_estudiante" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title titulo_modal">Vista de Solicitud No.</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="col-md-12 col-12">
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Nombre del estudiante:</h5>
+								<input type="text" class="form-control" id="txt_nombre_estudiante">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Carrera del estudiante:</h5>
+								<input type="text" class="form-control" id="txt_carrera_estudiante">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Solicitud:</h5>
+								<textarea class="form-control" rows="4" id="txt_solicitud_estudiante"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Motivos académicos:</h5>
+								<textarea class="form-control" rows="4" id="txt_motivos_academicos_estudiante"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Motivos personales:</h5>
+								<textarea class="form-control" rows="4" id="txt_motivos_personales_estudiante"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Otros:</h5>
+								<textarea class="form-control" rows="4" id="txt_motivos_otros_estudiante"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12 col-sm-12">
+							<div class="form-group">
+								<h5>Evidencias:</h5>
+								<input type="file" class="form-control" id="txt_evidencia">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal" id="modal_visualizar_solicitud_profesor" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title titulo_modal">Vista de Solicitud No.</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="col-md-12 col-12">
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Nombre del Profesor:</h5>
+								<input type="text" class="form-control" id="txt_nombre_profesor">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Carrera del Profesor:</h5>
+								<input type="text" class="form-control" id="txt_carrera_profesor">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Solicitud:</h5>
+								<textarea class="form-control" rows="4" id="txt_solicitud_profesor"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="form-group">
+								<h5>Motivos académicos:</h5>
+								<textarea class="form-control" rows="4" id="txt_motivos_academicos_profesor"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 col-12 col-sm-12">
+							<div class="form-group">
+								<h5>Evidencias:</h5>
+								<input type="file" class="form-control" id="txt_evidencia">
 							</div>
 						</div>
 					</div>
