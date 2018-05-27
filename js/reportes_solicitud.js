c0a9f60837fcdaa0b8a4b3e0c70d2b72926765e5
Diego Armando Payán López <DiegoGMR@users.noreply.github.com>
2018-05-27 17:06:08 -0600

    27/05/2018
    Tarea: Módulo de verificar estatus solicitud por interesado

--- js/reportes_solicitud.js
+++ js/reportes_solicitud.js
@@ -7,13 +7,22 @@ $(document).ready(function(){
 		success: function(data){
 			
 			$(data.data).each(function(i,v){
+				
+				var boton_accion = "";
+				
+				if(v.estatus == "Rechazada por coordinador por evidencia"){
+					boton_accion = '<td><button class="btn btn-info btn-sm" data-id="'+v.id_solicitud+'" id="btn_evidencia"><i class="fa fa-upload"></i> Subir evidencia</button></td>';
+				}else if(v.estatus == "Rechazada por coordinador con motivo" || v.estatus == "Rechazada por director del plantel"){
+					boton_accion = '<td><button class="btn btn-info btn-sm" id="btn_nueva_solicitud"><i class="fa fa-newspaper"></i> Nueva Solicitud</button></td>';
+				}
+				
 				$('.carga_solicitudes').append('<tr>'
 											  +'<td>'+v.id_solicitud+'</td>'
 											  +'<td>'+v.solicitud+'</td>'
 											  +'<td>'+v.fecha_creacion+'</td>'
 											  +'<td>'+v.ultima_modificacion+'</td>'
-											  +'<td>'+v.estatus+'</td>'
-											  +'<td><button class="btn btn-info btn-sm" data-id="'+v.id_solicitud+'" id="btn_actualizar"><i class="fa fa-sync"></i> Actualizar</button></td>'
+											  +'<td>'+v.estatus+'</td>'											  
+											  + boton_accion
 											+'</tr>');
 			});
 			
@@ -22,6 +31,27 @@ $(document).ready(function(){
 			console.log(xhr);
 			console.log("Descripcion: " + desc + "\nError: "  + err);
 		}
+	});
+	
+	$('.carga_solicitudes').on("click","#btn_evidencia",function(){
+		
+		alert('Picale Nico, aquí se puede subir evidencia tambien');
+		
+	});
+	
+	$('.carga_solicitudes').on("click","#btn_nueva_solicitud",function(){
+		
+		swal({
+			title: "Redireccionando",
+			text: "Modulo para dar de alta nueva solicitud",
+			type: "info",
+			timer: 2000
+		});
+		
+		setTimeout(function(){
+			cargarDiv('solicitud');
+		},1900);
+		
 	});
 	
 });
